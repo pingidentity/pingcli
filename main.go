0071f074b6b454a05326ff5cbf12179757482214
Erik Ostien <erikostien@pingidentity.com>
2024-10-18 10:24:52 +0200

    PDI-2056: Use GoReleaser to Apply PingCLI Version and Commit to Builds

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 	"github.com/pingidentity/pingcli/internal/output"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+	commit  string = "dev"
+)
+
 func main() {
-	rootCmd := cmd.NewRootCommand()
+	rootCmd := cmd.NewRootCommand(version, commit)
 
 	err := rootCmd.Execute()
 	if err != nil {
