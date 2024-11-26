fcb2312245ba031447053eec3f0b4e6947cc70f2
Erik Ostien <erikostien@pingidentity.com>
2024-11-26 11:12:27 -0700

    Update Protect resource exports to iter info extraction design

--- internal/connector/pingone/protect/resources/pingone_risk_policy.go
+++ internal/connector/pingone/protect/resources/pingone_risk_policy.go
@@ -14,58 +14,79 @@ var (
 )
 
 type PingOneRiskPolicyResource struct {
-	clientInfo *connector.PingOneClientInfo
+	clientInfo   *connector.PingOneClientInfo
+	importBlocks *[]connector.ImportBlock
 }
 
 // Utility method for creating a PingOneRiskPolicyResource
 func RiskPolicy(clientInfo *connector.PingOneClientInfo) *PingOneRiskPolicyResource {
 	return &PingOneRiskPolicyResource{
-		clientInfo: clientInfo,
+		clientInfo:   clientInfo,
+		importBlocks: &[]connector.ImportBlock{},
 	}
-}
-
-func (r *PingOneRiskPolicyResource) ExportAll() (*[]connector.ImportBlock, error) {
-	l := logger.Get()
-
-	l.Debug().Msgf("Fetching all %s resources...", r.ResourceType())
-
-	apiExecuteFunc := r.clientInfo.ApiClient.RiskAPIClient.RiskPoliciesApi.ReadRiskPolicySets(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute
-	apiFunctionName := "ReadRiskPolicySets"
-
-	embedded, err := common.GetProtectEmbedded(apiExecuteFunc, apiFunctionName, r.ResourceType())
-	if err != nil {
-		return nil, err
-	}
-
-	importBlocks := []connector.ImportBlock{}
-
-	l.Debug().Msgf("Generating Import Blocks for all %s resources...", r.ResourceType())
-
-	for _, riskPolicySet := range embedded.GetRiskPolicySets() {
-		riskPolicySetName, riskPolicySetNameOk := riskPolicySet.GetNameOk()
-		riskPolicySetId, riskPolicySetIdOk := riskPolicySet.GetIdOk()
-
-		if riskPolicySetNameOk && riskPolicySetIdOk {
-			commentData := map[string]string{
-				"Resource Type":         r.ResourceType(),
-				"Risk Policy Name":      *riskPolicySetName,
-				"Export Environment ID": r.clientInfo.ExportEnvironmentID,
-				"Risk Policy ID":        *riskPolicySetId,
-			}
-
-			importBlocks = append(importBlocks, connector.ImportBlock{
-				ResourceType:       r.ResourceType(),
-				ResourceName:       *riskPolicySetName,
-				ResourceID:         fmt.Sprintf("%s/%s", r.clientInfo.ExportEnvironmentID, *riskPolicySetId),
-				CommentInformation: common.GenerateCommentInformation(commentData),
-			})
-
-		}
-	}
-
-	return &importBlocks, nil
 }
 
 func (r *PingOneRiskPolicyResource) ResourceType() string {
 	return "pingone_risk_policy"
+}
+
+func (r *PingOneRiskPolicyResource) ExportAll() (*[]connector.ImportBlock, error) {
+	l := logger.Get()
+	l.Debug().Msgf("Exporting all '%s' Resources...", r.ResourceType())
+
+	err := r.exportPolicies()
+	if err != nil {
+		return nil, err
+	}
+
+	return r.importBlocks, nil
+}
+
+func (r *PingOneRiskPolicyResource) exportPolicies() error {
+	iter := r.clientInfo.ApiClient.RiskAPIClient.RiskPoliciesApi.ReadRiskPolicySets(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute()
+
+	for cursor, err := range iter {
+		err = common.HandleClientResponse(cursor.HTTPResponse, err, "ReadRiskPolicySets", r.ResourceType())
+		if err != nil {
+			return err
+		}
+
+		if cursor.EntityArray == nil {
+			return common.DataNilError(r.ResourceType(), cursor.HTTPResponse)
+		}
+
+		embedded, embeddedOk := cursor.EntityArray.GetEmbeddedOk()
+		if !embeddedOk {
+			return common.DataNilError(r.ResourceType(), cursor.HTTPResponse)
+		}
+
+		for _, riskPolicySet := range embedded.GetRiskPolicySets() {
+			riskPolicySetName, riskPolicySetNameOk := riskPolicySet.GetNameOk()
+			riskPolicySetId, riskPolicySetIdOk := riskPolicySet.GetIdOk()
+
+			if riskPolicySetIdOk && riskPolicySetNameOk {
+				r.addImportBlock(*riskPolicySetId, *riskPolicySetName)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (r *PingOneRiskPolicyResource) addImportBlock(riskPolicySetId, riskPolicySetName string) {
+	commentData := map[string]string{
+		"Export Environment ID": r.clientInfo.ExportEnvironmentID,
+		"Resource Type":         r.ResourceType(),
+		"Risk Policy ID":        riskPolicySetId,
+		"Risk Policy Name":      riskPolicySetName,
+	}
+
+	importBlock := connector.ImportBlock{
+		ResourceType:       r.ResourceType(),
+		ResourceName:       riskPolicySetName,
+		ResourceID:         fmt.Sprintf("%s/%s", r.clientInfo.ExportEnvironmentID, riskPolicySetId),
+		CommentInformation: common.GenerateCommentInformation(commentData),
+	}
+
+	*r.importBlocks = append(*r.importBlocks, importBlock)
 }
--- internal/connector/pingone/protect/resources/pingone_risk_predictor.go
+++ internal/connector/pingone/protect/resources/pingone_risk_predictor.go
@@ -15,123 +15,143 @@ var (
 )
 
 type PingOneRiskPredictorResource struct {
-	clientInfo *connector.PingOneClientInfo
+	clientInfo   *connector.PingOneClientInfo
+	importBlocks *[]connector.ImportBlock
 }
 
 // Utility method for creating a PingOneRiskPredictorResource
 func RiskPredictor(clientInfo *connector.PingOneClientInfo) *PingOneRiskPredictorResource {
 	return &PingOneRiskPredictorResource{
-		clientInfo: clientInfo,
+		clientInfo:   clientInfo,
+		importBlocks: &[]connector.ImportBlock{},
 	}
-}
-
-func (r *PingOneRiskPredictorResource) ExportAll() (*[]connector.ImportBlock, error) {
-	l := logger.Get()
-
-	l.Debug().Msgf("Fetching all %s resources...", r.ResourceType())
-
-	apiExecuteFunc := r.clientInfo.ApiClient.RiskAPIClient.RiskAdvancedPredictorsApi.ReadAllRiskPredictors(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute
-	apiFunctionName := "ReadAllRiskPredictors"
-
-	embedded, err := common.GetProtectEmbedded(apiExecuteFunc, apiFunctionName, r.ResourceType())
-	if err != nil {
-		return nil, err
-	}
-
-	importBlocks := []connector.ImportBlock{}
-
-	l.Debug().Msgf("Generating Import Blocks for all %s resources...", r.ResourceType())
-
-	for _, riskPredictor := range embedded.GetRiskPredictors() {
-		var (
-			riskPredictorId   *string
-			riskPredictorIdOk bool
-
-			riskPredictorName   *string
-			riskPredictorNameOk bool
-
-			riskPredictorType   *risk.EnumPredictorType
-			riskPredictorTypeOk bool
-		)
-
-		switch {
-		case riskPredictor.RiskPredictorAdversaryInTheMiddle != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetTypeOk()
-		case riskPredictor.RiskPredictorAnonymousNetwork != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorAnonymousNetwork.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorAnonymousNetwork.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorAnonymousNetwork.GetTypeOk()
-		case riskPredictor.RiskPredictorBotDetection != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorBotDetection.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorBotDetection.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorBotDetection.GetTypeOk()
-		case riskPredictor.RiskPredictorCommon != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorCommon.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorCommon.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorCommon.GetTypeOk()
-		case riskPredictor.RiskPredictorComposite != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorComposite.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorComposite.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorComposite.GetTypeOk()
-		case riskPredictor.RiskPredictorCustom != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorCustom.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorCustom.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorCustom.GetTypeOk()
-		case riskPredictor.RiskPredictorDevice != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorDevice.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorDevice.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorDevice.GetTypeOk()
-		case riskPredictor.RiskPredictorEmailReputation != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorEmailReputation.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorEmailReputation.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorEmailReputation.GetTypeOk()
-		case riskPredictor.RiskPredictorGeovelocity != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorGeovelocity.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorGeovelocity.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorGeovelocity.GetTypeOk()
-		case riskPredictor.RiskPredictorIPReputation != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorIPReputation.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorIPReputation.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorIPReputation.GetTypeOk()
-		case riskPredictor.RiskPredictorUserLocationAnomaly != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetTypeOk()
-		case riskPredictor.RiskPredictorUserRiskBehavior != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorUserRiskBehavior.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorUserRiskBehavior.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorUserRiskBehavior.GetTypeOk()
-		case riskPredictor.RiskPredictorVelocity != nil:
-			riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorVelocity.GetIdOk()
-			riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorVelocity.GetNameOk()
-			riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorVelocity.GetTypeOk()
-		default:
-			continue
-		}
-
-		if riskPredictorIdOk && riskPredictorNameOk && riskPredictorTypeOk {
-			commentData := map[string]string{
-				"Resource Type":         r.ResourceType(),
-				"Risk Predictor Type":   string(*riskPredictorType),
-				"Risk Predictor Name":   *riskPredictorName,
-				"Export Environment ID": r.clientInfo.ExportEnvironmentID,
-				"Risk Predictor ID":     *riskPredictorId,
-			}
-
-			importBlocks = append(importBlocks, connector.ImportBlock{
-				ResourceType:       r.ResourceType(),
-				ResourceName:       fmt.Sprintf("%s_%s", *riskPredictorType, *riskPredictorName),
-				ResourceID:         fmt.Sprintf("%s/%s", r.clientInfo.ExportEnvironmentID, *riskPredictorId),
-				CommentInformation: common.GenerateCommentInformation(commentData),
-			})
-		}
-	}
-
-	return &importBlocks, nil
 }
 
 func (r *PingOneRiskPredictorResource) ResourceType() string {
 	return "pingone_risk_predictor"
+}
+
+func (r *PingOneRiskPredictorResource) ExportAll() (*[]connector.ImportBlock, error) {
+	l := logger.Get()
+	l.Debug().Msgf("Exporting all '%s' Resources...", r.ResourceType())
+
+	err := r.exportRiskPredictors()
+	if err != nil {
+		return nil, err
+	}
+
+	return r.importBlocks, nil
+}
+
+func (r *PingOneRiskPredictorResource) exportRiskPredictors() error {
+	iter := r.clientInfo.ApiClient.RiskAPIClient.RiskAdvancedPredictorsApi.ReadAllRiskPredictors(r.clientInfo.Context, r.clientInfo.ExportEnvironmentID).Execute()
+
+	for cursor, err := range iter {
+		err = common.HandleClientResponse(cursor.HTTPResponse, err, "ReadAllRiskPredictors", r.ResourceType())
+		if err != nil {
+			return err
+		}
+
+		if cursor.EntityArray == nil {
+			return common.DataNilError(r.ResourceType(), cursor.HTTPResponse)
+		}
+
+		embedded, embeddedOk := cursor.EntityArray.GetEmbeddedOk()
+		if !embeddedOk {
+			return common.DataNilError(r.ResourceType(), cursor.HTTPResponse)
+		}
+
+		for _, riskPredictor := range embedded.GetRiskPredictors() {
+			var (
+				riskPredictorId     *string
+				riskPredictorIdOk   bool
+				riskPredictorName   *string
+				riskPredictorNameOk bool
+				riskPredictorType   *risk.EnumPredictorType
+				riskPredictorTypeOk bool
+			)
+
+			switch {
+			case riskPredictor.RiskPredictorAdversaryInTheMiddle != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorAdversaryInTheMiddle.GetTypeOk()
+			case riskPredictor.RiskPredictorAnonymousNetwork != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorAnonymousNetwork.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorAnonymousNetwork.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorAnonymousNetwork.GetTypeOk()
+			case riskPredictor.RiskPredictorBotDetection != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorBotDetection.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorBotDetection.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorBotDetection.GetTypeOk()
+			case riskPredictor.RiskPredictorCommon != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorCommon.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorCommon.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorCommon.GetTypeOk()
+			case riskPredictor.RiskPredictorComposite != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorComposite.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorComposite.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorComposite.GetTypeOk()
+			case riskPredictor.RiskPredictorCustom != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorCustom.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorCustom.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorCustom.GetTypeOk()
+			case riskPredictor.RiskPredictorDevice != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorDevice.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorDevice.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorDevice.GetTypeOk()
+			case riskPredictor.RiskPredictorEmailReputation != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorEmailReputation.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorEmailReputation.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorEmailReputation.GetTypeOk()
+			case riskPredictor.RiskPredictorGeovelocity != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorGeovelocity.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorGeovelocity.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorGeovelocity.GetTypeOk()
+			case riskPredictor.RiskPredictorIPReputation != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorIPReputation.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorIPReputation.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorIPReputation.GetTypeOk()
+			case riskPredictor.RiskPredictorUserLocationAnomaly != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorUserLocationAnomaly.GetTypeOk()
+			case riskPredictor.RiskPredictorUserRiskBehavior != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorUserRiskBehavior.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorUserRiskBehavior.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorUserRiskBehavior.GetTypeOk()
+			case riskPredictor.RiskPredictorVelocity != nil:
+				riskPredictorId, riskPredictorIdOk = riskPredictor.RiskPredictorVelocity.GetIdOk()
+				riskPredictorName, riskPredictorNameOk = riskPredictor.RiskPredictorVelocity.GetNameOk()
+				riskPredictorType, riskPredictorTypeOk = riskPredictor.RiskPredictorVelocity.GetTypeOk()
+			default:
+				continue
+			}
+
+			if riskPredictorIdOk && riskPredictorNameOk && riskPredictorTypeOk {
+				r.addImportBlock(*riskPredictorId, *riskPredictorName, string(*riskPredictorType))
+			}
+		}
+	}
+
+	return nil
+}
+
+func (r *PingOneRiskPredictorResource) addImportBlock(riskPredictorId, riskPredictorName, riskPredictorType string) {
+	commentData := map[string]string{
+		"Export Environment ID": r.clientInfo.ExportEnvironmentID,
+		"Resource Type":         r.ResourceType(),
+		"Risk Predictor ID":     riskPredictorId,
+		"Risk Predictor Name":   riskPredictorName,
+		"Risk Predictor Type":   riskPredictorType,
+	}
+
+	importBlock := connector.ImportBlock{
+		ResourceType:       r.ResourceType(),
+		ResourceName:       fmt.Sprintf("%s_%s", riskPredictorType, riskPredictorName),
+		ResourceID:         fmt.Sprintf("%s/%s", r.clientInfo.ExportEnvironmentID, riskPredictorId),
+		CommentInformation: common.GenerateCommentInformation(commentData),
+	}
+
+	*r.importBlocks = append(*r.importBlocks, importBlock)
 }
