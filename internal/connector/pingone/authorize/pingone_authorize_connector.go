87fe2e7bfd6d0621cb4f8a7f0c791a1d85eb7d9d
Patrick Cowland <patrickcowland@pingidentity.com>
2025-02-28 13:05:20 +0000

    add back api service deployment

--- internal/connector/pingone/authorize/pingone_authorize_connector.go
+++ internal/connector/pingone/authorize/pingone_authorize_connector.go
@@ -43,7 +43,7 @@ func (c *PingoneAuthorizeConnector) Export(format, outputDir string, overwriteEx
 
 	exportableResources := []connector.ExportableResource{
 		resources.AuthorizeAPIService(&c.clientInfo),
-		// resources.AuthorizeAPIServiceDeployment(&c.clientInfo),
+		resources.AuthorizeAPIServiceDeployment(&c.clientInfo),
 		resources.AuthorizeAPIServiceOperation(&c.clientInfo),
 		resources.ApplicationResource(&c.clientInfo),
 		resources.AuthorizeApplicationResourcePermission(&c.clientInfo),
