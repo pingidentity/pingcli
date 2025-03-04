095519da8430af45ea829f5d8c9c1825e5368826
wesleymccollam <wesleymccollam@pingidentity.com>
2025-03-04 17:24:24 -0500

    add test

--- internal/connector/pingone/sso/resources/pingone_population_default_identity_provider_test.go
+++ internal/connector/pingone/sso/resources/pingone_population_default_identity_provider_test.go
@@ -9,25 +9,15 @@ import (
 	"github.com/pingidentity/pingcli/internal/testing/testutils"
 )
 
-func TestPopulationExport(t *testing.T) {
+func TestPopulationDefaultIdpExport(t *testing.T) {
 	// Get initialized apiClient and resource
 	PingOneClientInfo := testutils.GetPingOneClientInfo(t)
-	resource := resources.Population(PingOneClientInfo)
+	resource := resources.PopulationDefaultIdp(PingOneClientInfo)
 
 	// Defined the expected ImportBlocks for the resource
 	expectedImportBlocks := []connector.ImportBlock{
 		{
-			ResourceType: "pingone_population",
-			ResourceName: "Default",
-			ResourceID:   fmt.Sprintf("%s/720da2ce-4dd0-48d9-af75-aeadbda1860d", testutils.GetEnvironmentID()),
-		},
-		{
-			ResourceType: "pingone_population",
-			ResourceName: "LDAP Gateway Population",
-			ResourceID:   fmt.Sprintf("%s/374fdb3c-4e94-4547-838a-0c200b9a7c70", testutils.GetEnvironmentID()),
-		},
-		{
-			ResourceType: "pingone_population",
+			ResourceType: "pingone_population_default_identity_provider",
 			ResourceName: "Test Default Idp Population",
 			ResourceID:   fmt.Sprintf("%s/2814912d-4a0f-4104-a779-80c13b2a6dcd", testutils.GetEnvironmentID()),
 		},
